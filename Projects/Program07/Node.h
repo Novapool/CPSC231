6b142de0508db4329002a57949c090beb460b114
Laith Assaf <laitho4325@gmail.com>
2025-04-24 16:12:35 -0400

    Add Node class declaration with constructors, destructor, and member variables

---
+++ Projects/Program07/Node.h
@@ -0,0 +1,28 @@
+/***********************************
+ * Node.h
+ * Written by Josh Lamb
+ ***********************************/
+
+ #ifndef NODE_H
+ #define NODE_H
+ 
+ #include <string>
+ using namespace std;
+ 
+ // Node class declaration
+ class node
+ {
+     friend class graph;
+     
+     public:
+         node();                 // Default constructor
+         node(string value);     // Constructor with string value
+         ~node();                // Destructor
+         
+     private:
+         string value;           // Stored value (a string)
+         bool visit;             // Boolean indicator shows if node has been visited
+         node* next;             // Pointer to the next node in the linked list
+ };
+ 
+ #endif // NODE_H
