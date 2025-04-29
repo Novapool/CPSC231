3ecea246e047fe47663e3cddb8d249e7dab892a7
Laith Assaf <laitho4325@gmail.com>
2025-04-29 17:25:28 -0400

    Refactor Lab23: Remove Lab23.h and update Main.cpp to use Lab23.h2, enhance city display, and improve pathfinding logic

--- Labs/Lab23/Main.cpp
+++ Labs/Lab23/Main.cpp
@@ -6,7 +6,7 @@
 #include <string>
 using namespace std;
 
-#include "Lab23.h"
+#include "Lab23.h2"
 
 /******************************
  * showCityList()
@@ -16,12 +16,16 @@ void showCityList()
 { int i;
 
   // Header
+  
   cout << "City List" << endl;
   cout << "--------------" << endl;
 
   // Display cities
+  
   for(i=0; i<CITY_MAX; i++)
-    cout << city[i] << endl;
+  
+  { cout << i << ") " << city[i] << endl;
+  };
     
   cout << endl;
 }
@@ -31,7 +35,7 @@ void showCityList()
  * Set the return path to 0 to prevent going back
  ******************************/
 void invalidateReturnPath(int map[][CITY_MAX], int from, int to)
-{
+{ 
   map[to][from] = 0;
 }
 
@@ -43,12 +47,18 @@ void invalidateCity(int map[][CITY_MAX], int cityIndex)
 { int i;
 
   // Mark all outgoing paths as invalid
+  
   for(i=0; i<CITY_MAX; i++)
-    map[cityIndex][i] = 0;
+  
+  { map[cityIndex][i] = 0;
+  };
 
   // Mark all incoming paths as invalid
+  
   for(i=0; i<CITY_MAX; i++)
-    map[i][cityIndex] = 0;
+  
+  { map[i][cityIndex] = 0;
+  };
 }
 
 /******************************
@@ -56,61 +66,87 @@ void invalidateCity(int map[][CITY_MAX], int cityIndex)
  * Calculate the path from start to stop city
  ******************************/
 void findPath(int start, int stop)
-{ int pos, temp, i;
+{ int pos, temp, i, j;
   int total = 0;
   int distance[CITY_MAX][CITY_MAX];
   bool reachedDestination = false;
 
-  // Copy the distance array since we'll be modifying it
+  // Copy the map array since we'll be modifying it
+  
   for(i=0; i<CITY_MAX; i++)
-    for(int j=0; j<CITY_MAX; j++)
-      distance[i][j] = DISTANCE[i][j];
+  
+  { for(j=0; j<CITY_MAX; j++)
+    
+    { distance[i][j] = map[i][j];
+    };
+  };
 
   // Start at the specified city
+  
   pos = start;
 
   // Loop until we reach destination or get stuck
+  
   while(pos != stop && pos < CITY_MAX)
+  
   {
     // Find the first available path from current city
+    
     temp = 0;
     while(temp < CITY_MAX && distance[pos][temp] == 0)
-      temp++;
+    
+    { temp++;
+    };
 
     // If we found a path
+    
     if(temp < CITY_MAX)
+    
     {
       // Display and add the distance to total
-      cout << city[pos].substr(3) << " to " << city[temp].substr(3) << " = " << distance[pos][temp] << endl;
+      
+      cout << city[pos] << " to " << city[temp] << " = " << distance[pos][temp] << endl;
       total += distance[pos][temp];
 
       // Invalidate the path we just took
+      
       distance[pos][temp] = 0;
       
       // Invalidate the return path to prevent going back
+      
       invalidateReturnPath(distance, pos, temp);
       
       // Invalidate the city we're leaving
+      
       invalidateCity(distance, pos);
       
       // Move to the next city
+      
       pos = temp;
       
       // Check if we reached the destination
+      
       if(pos == stop)
-        reachedDestination = true;
+      
+      { reachedDestination = true;
+      };
     }
     else
+    
     {
       // No path found, we're stuck
+      
       cout << "Can't get there from here" << endl;
-      break;
-    }
-  }
+      pos = CITY_MAX;  // Set pos to exit the loop instead of using break
+    };
+  };
 
   // Display total distance if we reached destination
+  
   if(reachedDestination)
-    cout << "Total = " << total << endl;
+  
+  { cout << "Total = " << total << endl;
+  };
 }
 
 /******************************
@@ -121,9 +157,11 @@ int main()
 { int start, stop;
 
   // Show list of cities
+  
   showCityList();
 
   // Get starting and ending cities
+  
   cout << "Enter start city: ";
   cin >> start;
   
@@ -132,15 +170,19 @@ int main()
   cout << endl;
 
   // Validate input
+  
   if(start < 0 || start >= CITY_MAX || stop < 0 || stop >= CITY_MAX)
+  
   {
     cout << "Invalid city selection" << endl;
     return 1;
-  }
+  };
 
   // Find path between cities
+  
   findPath(start, stop);
 
   // Success
+  
   return 0;
 }
